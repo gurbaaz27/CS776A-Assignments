8180c801b0b3cbc1655734b5b2acf5f34e23039c
gurbaaz <gurbaazsn123@gmail.com>
2022-01-22 17:53:44 +0530

    Add numpy

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -9,6 +9,7 @@ import sys
 import random
 import logging
 import pickle
+import numpy as np
 
 
 def enable_logging() -> logging.Logger:
@@ -41,7 +42,7 @@ def unpickle(file):
     return dict
 
 
-def random_rotation(input_image):
+def random_rotation(input_image, log):
     """
     Random Rotation in the range [−180 degree, +180 degree]
 
@@ -50,10 +51,12 @@ def random_rotation(input_image):
     Output:
         Transformed image
     """
-    pass
+    theta = random.randint(-180, 180)
+
+    log.info(f"Rotating the image by {theta} degrees")
 
 
-def random_cutout(input_image):
+def random_cutout(input_image, log):
     """
     Random cutout (randomly erase a block of pixels from the image with the width and height
     of the block in the range 0 to 16 pixels. The erased part (cutout) should be filled with a
@@ -67,7 +70,7 @@ def random_cutout(input_image):
     pass
 
 
-def random_crop(input_image):
+def random_crop(input_image, log):
     """
     Random Crop (Add a padding of 2 pixels on all sides and randomly select a block of 32x32
     pixels from the padded image)
@@ -80,7 +83,7 @@ def random_crop(input_image):
     pass
 
 
-def contrast_and_horizontal_flipping(input_image):
+def contrast_and_horizontal_flipping(input_image, log):
     """
     Contrast & Horizontal flipping. (First, change the contrast of the image with a factor of
     α randomly selected from the range (0.5, 2.0) and then flip the image horizontally with a
@@ -117,29 +120,62 @@ def main():
             for i in range(IMAGES_PER_BATCH):
                 filenames.append(batch_dataset[b"filenames"][i])
                 labels.append(batch_dataset[b"labels"][i])
-                images.append(batch_dataset[b"data"][i])
+                images.append(np.array(batch_dataset[b"data"][i]).reshape(3, 1024))
 
         elif file.startswith("batches"):
             label_names = unpickle(os.path.join(CIFAR_DATASET_FILENAME, file))[
                 b"label_names"
             ]
+            label_names = [label.decode("utf-8") for label in label_names]
+        
+        elif file.startswith("test_batch"):
+            test_batch = unpickle(os.path.join(CIFAR_DATASET_FILENAME, file))
+            test_dataset = {"filenames": [], "labels": [], "images": []}
+
+            for i in range(IMAGES_PER_BATCH):
+                test_dataset["filenames"].append(test_batch[b"filenames"][i])
+                test_dataset["labels"].append(test_batch[b"labels"][i])
+                test_dataset["images"].append(np.array(test_batch[b"data"][i]).reshape(3, 1024))
 
     train_dataset = {"filenames": filenames, "labels": labels, "images": images}
 
-    log.info(len(train_dataset["filenames"]))
-    log.info(label_names)
+    log.info(f"Size of train dataset: {len(train_dataset['filenames'])}")
+    log.info(f"Size of test dataset: {len(test_dataset['filenames'])}")
+    log.info(f"Labels in dataset: {label_names}")
 
     ## 2. Image transformations
 
     example_idx = random.randint(0, NUM_TRAIN_BATCHES * IMAGES_PER_BATCH - 1)
     example_image = train_dataset["images"][example_idx]
 
-    random_rotation(example_image)
-    random_cutout(example_image)
-    random_crop(example_image)
-    contrast_and_horizontal_flipping(example_image)
+    log.info(f"Example test image is {train_dataset['filenames'][example_idx].decode('utf-8')}")
 
+    random_rotation(example_image, log)
+    random_cutout(example_image, log)
+    random_crop(example_image, log)
+    contrast_and_horizontal_flipping(example_image, log)
+
+    return 0
     ## 3. Generating augmented training set
+
+    for i in range(NUM_TRAIN_BATCHES * IMAGES_PER_BATCH):
+        operation = random.randint(0, 3)
+        og_image = train_dataset["images"][i]
+
+        if operation == 0:
+            augmented_image = random_rotation(og_image, log)
+        elif operation == 1:
+            augmented_image = random_cutout(og_image, log)
+        elif operation == 2:
+            augmented_image = random_crop(og_image, log)
+        else:
+            augmented_image = contrast_and_horizontal_flipping(og_image, log)
+
+        train_dataset["filenames"].append(b"augmented_" + train_dataset["filenames"][i])
+        train_dataset["labels"].append(train_dataset["labels"][i])
+        train_dataset["images"].append(augmented_image)
+
+    log.info(len(train_dataset["filenames"]))
 
     ## 4. Feature extraction
 
